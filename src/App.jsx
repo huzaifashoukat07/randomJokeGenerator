619d6c3e86d452339544cca3a6172e406b4966c9
Huzaifa Shoukat <huzafa459@gmail.com>
2025-08-08 01:42:38 +0500

    Initial commit of my joke generator app

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const url = "https://official-joke-api.appspot.com/random_joke";
+  const [joke, setJoke] = useState("");
+  // const [punchline, setPunchline] = useState("");
+  const fetchJoke = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setJoke(;
+      // setPunchline(`${data.punchline}  😂😂`);
+    } catch (error) {
+      console.error("Error fetching joke:", error);
+      setJoke("Failed to fetch a joke. Please try again later.");
+    }
+  };
+  return (
+    <div className="App">
+      <h1>Random Joke Generator</h1>
+      <button onClick={fetchJoke}>Next Joke</button>
+      <p>
+        {joke.setup} <br />
+      </p>
+      <p>{joke.punchline}</p>
+    </div>
+  );
+}
+
+export default App;
